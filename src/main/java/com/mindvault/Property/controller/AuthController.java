49cc47f2eca031806565653388ea52a49eb5da0c
Lyka <bin.lyka@institute.pse.ngo>
2025-12-31 09:19:26 +0700

    update login and register

--- src/main/java/com/mindvault/Property/controller/AuthController.java
+++ src/main/java/com/mindvault/Property/controller/AuthController.java
@@ -1,28 +1,32 @@
 package com.mindvault.Property.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.mindvault.Property.dtos_request.RegisterRequest;
-import com.mindvault.Property.dtos_respone.RegisterResponse;
+import com.mindvault.Property.entities.User;
 import com.mindvault.Property.services.AuthService;
-
-import lombok.RequiredArgsConstructor;
+import com.mindvault.Property.dtos_request.LoginRequest;
+import com.mindvault.Property.dtos_request.RegisterRequest;
+import com.mindvault.Property.dtos_respone.AuthResponse; // THIS IMPORT WAS MISSING
 
 @RestController
 @RequestMapping("/api/auth")
-@RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService authService;
+    @Autowired
+    private AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponse> register(
-            @RequestBody RegisterRequest request) {
+    public ResponseEntity<User> registerUser(@RequestBody RegisterRequest request) {
+        User newUser = authService.register(request);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
 
-        return ResponseEntity.ok(authService.register(request));
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.status(response.getStatus()).body(response);
     }
 }
--- src/main/java/com/mindvault/Property/repositories/UserRepository.java
+++ src/main/java/com/mindvault/Property/repositories/UserRepository.java
@@ -1,13 +1,12 @@
 package com.mindvault.Property.repositories;
 
 import com.mindvault.Property.entities.User;
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
-    
+    Boolean existsByEmail(String email);
 }
